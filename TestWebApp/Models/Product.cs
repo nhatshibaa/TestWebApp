1dd826d55eec885806e43364d2ce0eddede47068
nhatshibaa <nhatntth2008069@fpt.edu.vn>
2021-11-16 20:58:10 +0700

    Add project files.

---
+++ TestWebApp/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace TestWebApp.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public int Cate_id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Unit { get; set; }
+    }
+}
